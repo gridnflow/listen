0b4735afa38c30d20c1275d90aaf117888002178
yeong.wi <wi10004ok@gmail.com>
2026-05-01 15:02:22 +0900

    refactor: remove unused AppConstants class and empty directories

--- lib/core/constants/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String appName = 'Listen';
-  static const String audioSubDir = 'audio';
-  static const String thumbnailSubDir = 'thumbnails';
-  static const int maxConcurrentDownloads = 3;
-}
